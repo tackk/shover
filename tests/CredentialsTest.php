91ff528de817c710748f2f60c8289ea251999914
Dan Horrigan <dan@dhorrigan.com>
2014-02-28 14:27:53 -0500

    Complete Credentials tests and ignore vendor from test coverage.

--- tests/CredentialsTest.php
+++ tests/CredentialsTest.php
@@ -3,9 +3,23 @@
 use Tackk\Shover\Credentials;
 
 class CredentialsTest extends PHPUnit_Framework_TestCase {
-	public function testCanBeInstantiated() {
-		$credentials = new Credentials('3', '278d425bdf160c739803', '7ad3773142a6692b25b8');
+	public function setUp() {
+		$this->credentials = new Credentials('3', '278d425bdf160c739803', '7ad3773142a6692b25b8');
+	}
 
-		$this->assertInstanceOf('Tackk\Shover\Credentials', $credentials);
+	public function testCanBeInstantiated() {
+		$this->assertInstanceOf('Tackk\Shover\Credentials', $this->credentials);
+	}
+
+	public function testGetAppId() {
+		$this->assertEquals('3', $this->credentials->getAppId());
+	}
+
+	public function testGetAuthKey() {
+		$this->assertEquals('278d425bdf160c739803', $this->credentials->getAuthKey());
+	}
+
+	public function testGetAuthSecret() {
+		$this->assertEquals('7ad3773142a6692b25b8', $this->credentials->getAuthSecret());
 	}
 }
